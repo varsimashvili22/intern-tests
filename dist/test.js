edc2618cb8ffc9c86323fb792385783463d9617e
varsimashvili22 <vanikonanuka@gmail.com>
2024-11-17 22:30:56 +0400

    მეტად გასუფთავებული

--- dist/test.js
+++ dist/test.js
@@ -1,13 +1,11 @@
-const indexPointer = "./index.js";
-import(indexPointer)
-    .then((myIndexElements) => {
+const indexPointer = "./index.js"; //დინამიურად, ანუ ფაილის სახელი განსაზღვროთ ცვლადით.
+async function loadModule() {
+    const myIndexElements = await import(indexPointer);
     myIndexElements.tellMeYourNameAndAge(); //chemi saxelia vaniko var 17 wlis
     myIndexElements.changeAge(71); //sheni axali asakia 71
     myIndexElements.tellMeYourNameAndAge(); //chemi saxelia vaniko var 71 wlis
     console.log(myIndexElements.name); //vaniko
     console.log(myIndexElements.squaredNumber(5)); //25
-})
-    .catch((error) => {
-    console.error("Error importing module:", error);
-});
+}
+loadModule();
 // დასატესტად გამოიყენეთ npm start
--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,9 @@
 
 const indexPointer = "./index.js"; //დინამიურად, ანუ ფაილის სახელი განსაზღვროთ ცვლადით.
   
-import(indexPointer)
-  .then((myIndexElements) => {
+async function loadModule() 
+{
+    const myIndexElements = await import(indexPointer);
     myIndexElements.tellMeYourNameAndAge(); //chemi saxelia vaniko var 17 wlis
 
     myIndexElements.changeAge(71); //sheni axali asakia 71
@@ -12,9 +13,7 @@ import(indexPointer)
     console.log(myIndexElements.name);//vaniko
     
     console.log(myIndexElements.squaredNumber(5));//25
-  })
-  .catch((error) => {
-    console.error("Error importing module:", error);
-  });
+}
+loadModule();
 
 // დასატესტად გამოიყენეთ npm start
