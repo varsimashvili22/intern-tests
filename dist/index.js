fcae2fc9806670077ab23a4e9a9f3b5470acaf00
varsimashvili22 <vanikonanuka@gmail.com>
2025-01-27 14:53:50 +0400

    დროის სარტყელის ტესტი რეგექსით

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,36 @@
-export let name = "vaniko";
-export let age = 17;
-export function tellMeYourNameAndAge() {
-    console.log(`chemi saxelia ${name} var ${age} wlis`);
-}
-export function squaredNumber(a) {
-    return a * a;
-}
-export function changeAge(newAge) {
-    age = newAge;
-    console.log("sheni axali asakia " + newAge);
-}
-export function changeName(newName) {
-    name = newName;
-    console.log("sheni axali saxelia " + newName);
+export function isTimeZoneValid(zone) {
+    const match = zone.match((/^(\+(?<positiveHours>(0\d|1[0-4])):(?<positiveMinutes>(00|30|45)))$|^((\-(?<negativeHours>(0\d)|(1[0-2])):(?<negativeMinutes>(00|30))))$/));
+    ;
+    const posHours = Number(match?.groups.positiveHours);
+    const posMinutes = Number(match?.groups.positiveMinutes);
+    const negHours = Number(match?.groups.negativeHours);
+    const negMinutes = Number(match?.groups.negativeMinutes);
+    if (!match) {
+        return false;
+    }
+    if (posHours > 14 || negHours > 12) {
+        return false;
+    }
+    if (!isNaN(posMinutes) && posMinutes != 0) {
+        if (posMinutes == 30) { //'+03:30', '+04:30', '+05:30', '+06:30', '+09:30', '+10:30', 
+            if ([3, 4, 5, 6, 9, 10].includes(posHours)) {
+                return true;
+            }
+        }
+        if (posMinutes == 45) { //'+08:45', '+05:45', '+12:45', '+13:45'
+            if ([5, 8, 12, 13].includes(posHours)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    if (!isNaN(negMinutes) && negMinutes != 0) {
+        if (negMinutes == 30) { //'-09:30', '-03:30', '-02:30', 
+            if ([2, 3, 9].includes(negHours)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return true;
 }
--- dist/test.js
+++ dist/test.js
@@ -1,11 +1,20 @@
-const indexPointer = "./index.js"; //დინამიურად, ანუ ფაილის სახელი განსაზღვროთ ცვლადით.
-async function loadModule() {
-    const myIndexElements = await import(indexPointer);
-    myIndexElements.tellMeYourNameAndAge(); //chemi saxelia vaniko var 17 wlis
-    myIndexElements.changeAge(71); //sheni axali asakia 71
-    myIndexElements.tellMeYourNameAndAge(); //chemi saxelia vaniko var 71 wlis
-    console.log(myIndexElements.name); //vaniko
-    console.log(myIndexElements.squaredNumber(5)); //25
-}
-loadModule();
-// დასატესტად გამოიყენეთ npm start
+import * as assert from "assert";
+import { describe, it } from 'mocha';
+import { isTimeZoneValid } from "./index.js";
+describe("დროის სარტყელი", () => {
+    it('მაგალითი', () => {
+        ['+', '-'].forEach(sign => {
+            for (let i = 0; i < 24; i++) {
+                for (let j = 0; j < 60; j++) {
+                    const hourText = i < 10 ? '0' + i : i;
+                    const minuteText = j < 10 ? '0' + j : j;
+                    const zonetext = sign + hourText + ':' + minuteText;
+                    const shouldBeTrue = ['+00:00', '+01:00', '+02:00', '+03:00', '+04:00', '+05:00', '+06:00', '+07:00', '+08:00', '+09:00', '+10:00', '+11:00', '+12:00', '+13:00', '+14:00', '-00:00',
+                        '-01:00', '-02:00', '-03:00', '-04:00', '-05:00', '-06:00', '-07:00', '-08:00', '-09:00', '-10:00', '-11:00', '-12:00',
+                        '-09:30', '-03:30', '-02:30', '+03:30', '+04:30', '+05:30', '+06:30', '+09:30', '+10:30', '+08:45', '+05:45', '+12:45', '+13:45'].includes(zonetext);
+                    assert.strictEqual(isTimeZoneValid(zonetext), shouldBeTrue);
+                }
+            }
+        });
+    });
+});
